15f2da8447ee7a5800307ed60decd5f8521757fb
Ali-CPP <ali.eldeen008@gmail.com>
2024-10-05 02:30:46 +0300

    Add files via upload

--- Nasa project/main.js
+++
@@ -1,9 +0,0 @@
-const trigger1 = document.querySelector('.3ah');
-
-const goAway = (nom, x, y) => {
-    nom.style.translate = `${x}vw ${y}vw`;
-}
-
-trigger1.addEventListener('mouseover', () => {
-    goAway(trigger1, -100, 100);
-})
